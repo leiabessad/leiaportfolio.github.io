fff0c8062148020ee7e21f879f25aa3657f6c85f
leiabessad <125222114+leiabessad@users.noreply.github.com>
2026-04-23 15:28:36 +0200

    Create navigation.js

---
+++ js/navigation.js
@@ -0,0 +1,13 @@
+const projects = [
+  "hellmann.html",
+  "Talentueux.html",
+  "femmesphotos.html",
+  "nefza.html",
+  "fenty.html",
+  "DMW.html",
+  "VCF.html",
+  "laika.html",
+  "Easylove.html",
+  "martine.html",
+  "linkee.html"
+];
